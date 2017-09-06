92ae7e4dce98ae46d3480ff6d6037a04d325281d
Sisira Kumara <skumara.wm@gmail.com>
2017-09-06 15:15:55 +0530

    Update test_sensor_reading_class.py

--- test_sensor_reading_class.py
+++ test_sensor_reading_class.py
@@ -8,9 +8,9 @@ while (True):
   temp2 = src1.find_true_temp(adc.read_voltage(3))
   RH2 = src1.find_trueRH_sr2(adc.read_voltage(4),temp2) #send two parameters rh_sensor voltage & temprature
   
-  print("Temprature sr1 :", round(temp1,2))
-  print("RH sr1 :", round(RH1,2))
-  print("Temprature sr2 :", round(temp1,2))
-  print("RH sr1 :", round(RH2,2))
+  print("Temprature sr1 :", int(round(temp1)))
+  print("RH sr1 :", int(round(RH1)))
+  print("Temprature sr2 :", int(round(temp1)))
+  print("RH sr1 :", int(round(RH2)))
   print(time.strftime("%H:%M:%S")) # print current system time
   time.sleep(1) # delay for one second
