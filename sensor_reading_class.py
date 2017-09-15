3e2b4c043218fcaaf2d941ffd1ed58e3e8e40c53
Sisira Kumara <skumara.wm@gmail.com>
2017-09-15 10:55:49 +0530

    Update sensor_reading_class.py

--- sensor_reading_class.py
+++ sensor_reading_class.py
@@ -18,7 +18,7 @@ class sensor_data:
     slope1 = 0.0301
     slope2 = 0.0295
     def find_true_temp(self,input_temp_voltage):
-        true_temp = input_temp_voltage * 243
+        true_temp = input_temp_voltage * 230
         return true_temp
     def find_trueRH_sr1(self,input_RH_voltage,input_temp):
         real_vout = (99.6+94.1)/(94.1)*input_RH_voltage*10.4
@@ -26,7 +26,7 @@ class sensor_data:
         true_rh = rh / (1.0546-(input_temp*0.00216))
         return true_rh
     def find_trueRH_sr2(self,input_RH_voltage,input_temp):
-        real_vout = (99.4+95.2)/(95.2)*input_RH_voltage*11.3
+        real_vout = (99.4+95.2)/(95.2)*input_RH_voltage*10.4
         rh = (real_vout - 0.74) / self.slope1
         true_rh = rh / (1.0546-(input_temp*0.00216))
         return true_rh
